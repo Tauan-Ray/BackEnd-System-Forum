98eb075ad3b456c0a01af3f938b8824127557ee5
Tauan-Ray <tauanray995@gmail.com>
2025-10-08 22:22:55 -0300

    :sparkles: feat: Adicionado prisma e model de user

---
+++ src/config/env.ts
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+import * as env from 'env-var';
+
+export const serverConfig = {
+  url: env.get('FORUM_DEFAULT_URL').default('localhost:3000').asUrlString(),
+  port: env.get('FORUM_SERVICE_PORT').default('3000').asPortNumber(),
+  env: env.get('NODE_ENV').default('development').asString(),
+};
+
+export const secret = {
+  JWT_SECRET_KEY: env.get('JWT_SECRET_KEY').required().asString(),
+};
